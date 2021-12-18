e1f1936c087aa79ff6a6c03a8b6ddb974784f158
ysilkov <65254365+ysilkov@users.noreply.github.com>
2021-12-18 18:14:30 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const myElement = document.querySelector("contact")
-myElement.addEventListener('click', function (event) {
-    console.log(event.type + ' got fired')
-   })
-   
-   myElement.addEventListener('click', function (event) {
-    console.log(event.type + ' got fired again')
-   })
